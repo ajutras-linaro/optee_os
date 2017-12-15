37c065480cfb6fbeebdc1ba4686e1637b90654dc
Edison Ai <edison.ai@arm.com>
2017-12-15 16:38:30 +0800

    libmbedtls: support mbedTLS in kernel mode
    Initial step of mbedtls cryptos integration.
    Directory created and interface file is drafted.
    All function interfaces are set to "not supported".
    The mbedtls can be selected by specifying build flags
    "CFG_CRYPTOLIB_NAME=mbedtls" and "CFG_CRYPTOLIB_DIR=lib/libmbedtls"
    
    Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Edison Ai <edison.ai@arm.com>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- lib/libmbedtls/include/mbedtls_config_kernel.h
+++ lib/libmbedtls/include/mbedtls_config_kernel.h
@@ -1,3 +1,24 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /* Copyright (c) 2018, Linaro Limited */
-#error "mbedTLS is not yet supported in kernel mode"
+#ifndef __MBEDTLS_CONFIG_KERNEL_H
+#define __MBEDTLS_CONFIG_KERNEL_H
+
+#ifdef CFG_CORE_MBEDTLS_MPI
+#ifdef ARM32
+#define MBEDTLS_HAVE_INT32
+#endif
+#ifdef ARM64
+#define MBEDTLS_HAVE_INT64
+#endif
+#define MBEDTLS_BIGNUM_C
+#define MBEDTLS_GENPRIME
+#endif
+
+/* Test if Mbedtls is the primary crypto lib */
+#ifdef CFG_CRYPTOLIB_NAME_mbedtls
+
+#endif /*CFG_CRYPTOLIB_NAME_mbedtls*/
+
+#include <mbedtls/check_config.h>
+
+#endif /* __MBEDTLS_CONFIG_KERNEL_H */

d9445e78994e84e5e9879d9af1bfd0710574fb04
Olivier Masse <olivier.masse@nxp.com>
2020-03-04 15:04:34 +0100

    core: driver: crypto add support MD5 hashes in RSA sign/verify
    Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

--- core/drivers/crypto/crypto_api/acipher/rsassa.c
+++ core/drivers/crypto/crypto_api/acipher/rsassa.c
@@ -885,6 +885,7 @@ TEE_Result rsassa_sign(struct drvcrypt_rsa_ssa *ssa_data)
 	case TEE_ALG_RSASSA_PKCS1_V1_5_SHA512:
 		return rsassa_pkcs1_v1_5_sign(ssa_data);
 
+	case TEE_ALG_RSASSA_PKCS1_PSS_MGF1_MD5:
 	case TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA1:
 	case TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA224:
 	case TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA256:
@@ -911,6 +912,7 @@ TEE_Result rsassa_verify(struct drvcrypt_rsa_ssa *ssa_data)
 	case TEE_ALG_RSASSA_PKCS1_V1_5_SHA512:
 		return rsassa_pkcs1_v1_5_verify(ssa_data);
 
+	case TEE_ALG_RSASSA_PKCS1_PSS_MGF1_MD5:
 	case TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA1:
 	case TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA224:
 	case TEE_ALG_RSASSA_PKCS1_PSS_MGF1_SHA256:
